60ca96df0d8c94d6366823013711c6377649bd89
MAYANK-TRIPATH <mr.mayanktripathi2001@gmail.com>
2025-02-10 14:41:31 +0530

    INit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div className='font-bold text-red-800'>
+        Init
+        </div>
+    </>
+  )
+}
+
+export default App
